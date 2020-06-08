6ecdbc5672637b3bb944c1f5911a50251fcf4452
dizsart <darogadibia@gmail.com>
2020-06-08 17:29:42 +0100

    add initial files to the project struvture

---
+++ src/main/java/com/cicd/springcicd/SpringcicdApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.springcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cicd/springcicd/SpringcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.springcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
